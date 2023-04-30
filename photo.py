b1194541f228254d6b823acf4246ef3f777a55a5
notunrandom <notunrandom@proton.me>
2023-04-30 16:45:12 +0200

    Added functions to produce file and directory names from datetime objects.

--- photo.py
+++ photo.py
@@ -1,8 +1,34 @@
 from datetime import datetime
 
-def prepend_datetime(name, dt, dsep='.', tsep='.', sep='-'):
-    return (
-        f'{dt.year}{dsep}{dt.month:02}{dsep}{dt.day:02}{sep}'
-        f'{dt.hour:02}{tsep}{dt.minute:02}{tsep}{dt.second:02}{sep}'
-        f'{name}'
-    )
+def iso8601(dt):
+    '''Convert a datetime to basic ISO 8601 format, e.g. 19840329T123300.
+
+    At this time datetime.isoformat() can only generate the extended ISO 8601 format.
+    The extended format contains colons. Colons are not valid in filenames on Mac OS.
+    This function produces a standard ISO 8601 datetime string which is usable in
+    file and directory names.
+    '''
+    return f'{dt.year:04}{dt.month:02}{dt.day:02}T{dt.hour:02}{dt.minute:02}{dt.second:02}'
+
+def iso8601_ext(dt, precision='day'):
+    '''Convert a datetime to extended ISO 8601 format, e.g. 1984-03-29.
+
+    At this time datetime.isoformat() does not provide the option of specifying precision.
+    '''
+    result = f'{dt.year:04}'
+    if precision != 'year':
+        result += f'-{dt.month:02}'
+        if precision != 'month':
+            result += f'-{dt.day:02}'
+    return result
+
+class IllegalSeparator(Exception):
+    pass
+
+def time_stamp_file_name(name, dt, sep='-'):
+    if not set(sep) <= set('-_.'): raise IllegalSeparator
+    return iso8601(dt) + sep + name
+
+def path_from_datetime(dt):
+    return '/'.join([iso8601_ext(dt, precision=p) for p in ['year', 'month']])
+
--- photo_test.py
+++ photo_test.py
@@ -1,11 +1,30 @@
-import os
 from datetime import datetime
 import photo
+import pytest
 
-def test_prepend_datetime():
+def test_iso8601():
+    assert photo.iso8601(datetime(1987,  3,  5,  0, 12, 59)) == "19870305T001259"
+    assert photo.iso8601(datetime(   1,  1,  1,  0,  0    )) == "00010101T000000"
+    assert photo.iso8601(datetime(9999, 12, 31, 23, 59, 59)) == "99991231T235959"
+    assert photo.iso8601(datetime(1987,  3,  5,  0, 12, 59, 359)) == "19870305T001259"
+
+def test_iso8601_ext():
+    assert photo.iso8601_ext(datetime(1987,  3,  5,  0, 12, 59)) == "1987-03-05"
+    assert photo.iso8601_ext(datetime(   1,  1,  1,  0,  0    )) == "0001-01-01"
+    assert photo.iso8601_ext(datetime(1987,  3,  5,  0, 12, 59), precision='month') == "1987-03"
+    assert photo.iso8601_ext(datetime(1987,  3,  5,  0, 12, 59), precision='year')  == "1987"
+
+def test_time_stamp_file_name():
     name = "NDX_1234.RAW" 
     dt = datetime(2010, 4, 15, 16, 22, 9)
-    new_name = photo.prepend_datetime(name, dt)
-    assert new_name == "2010.04.15-16.22.09-NDX_1234.RAW"
-    new_name = photo.prepend_datetime(name, dt, dsep='-', tsep=':', sep='--')
-    assert new_name == "2010-04-15--16:22:09--NDX_1234.RAW"
+    new_name = photo.time_stamp_file_name(name, dt)
+    assert new_name == "20100415T162209-NDX_1234.RAW"
+    new_name = photo.time_stamp_file_name(name, dt, sep='___')
+    assert new_name == "20100415T162209___NDX_1234.RAW"
+    with pytest.raises(photo.IllegalSeparator):
+        new_name = photo.time_stamp_file_name(name, dt, sep=':')
+
+def test_path_from_datetime():
+    dt = datetime(1995, 3, 17, 9, 5, 59)
+    path = photo.path_from_datetime(dt)
+    assert path == "1995/1995-03"
